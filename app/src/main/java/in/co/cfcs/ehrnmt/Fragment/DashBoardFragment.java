8986b83097e4135548441853f5d0d28de213d7f7
ANDRIOD\Android <kriteshmaurya91@gmail.com>
2018-08-02 18:31:28 +0530

    Bar insert

--- app/src/main/java/in/co/cfcs/ehrnmt/Fragment/DashBoardFragment.java
+++ app/src/main/java/in/co/cfcs/ehrnmt/Fragment/DashBoardFragment.java
@@ -12,6 +12,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 import in.co.cfcs.ehrnmt.Main.AttendanceModule;
 import in.co.cfcs.ehrnmt.Main.NewAddLeaveMangementActivity;
 import in.co.cfcs.ehrnmt.R;
@@ -35,6 +49,18 @@ public class DashBoardFragment extends Fragment {
     public LinearLayout leaverequsLay, attendanceLay, stationaryLay,docsLay,cabLay,hotelLay,appreceationLay,warningLay;
 
     public OnFragmentInteractionListenerForToolbar mListener;
+
+    PieChart pieChart,pieChart11 ;
+    ArrayList<Entry> entries ;
+    ArrayList<String> PieEntryLabels ;
+    PieDataSet pieDataSet ;
+    PieData pieData ;
+
+    BarChart barchart;
+    BarData barData;
+    BarDataSet barDataSet;
+    ArrayList<String> BarEntryLable;
+    ArrayList<BarEntry> entriesbar;
 
     public DashBoardFragment() {
         // Required empty public constructor
@@ -90,7 +116,7 @@ public class DashBoardFragment extends Fragment {
         cabLay = (LinearLayout)rootView.findViewById(R.id.cab_lay);
         hotelLay = (LinearLayout)rootView.findViewById(R.id.hotel_lay);
         appreceationLay = (LinearLayout)rootView.findViewById(R.id.appre_lay);
-        warningLay = (LinearLayout)rootView.findViewById(R.id.warning_lay);
+//        warningLay = (LinearLayout)rootView.findViewById(R.id.warning_lay);
 
         attendanceLay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -216,26 +242,26 @@ public class DashBoardFragment extends Fragment {
             }
         });
 
-        warningLay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mListener.onFragmentInteractionForToolbarMethod(221,"Warning",strtext);
-
-                FragmentManager fragmentManager = getFragmentManager();
-                Fragment frag = new WarningFragment();
-                frag.setArguments(bundle);
-
-                /*fragmentManager.setCustomAnimations(
-                        R.anim.push_right_in,
-                        R.anim.push_left_out, R.anim.push_left_in, R.anim.push_right_out);*/
-                // update the main content by replacing fragments
-                fragmentManager.beginTransaction()
-                        .replace(R.id.container, frag)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
+//        warningLay.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                mListener.onFragmentInteractionForToolbarMethod(221,"Warning",strtext);
+//
+//                FragmentManager fragmentManager = getFragmentManager();
+//                Fragment frag = new WarningFragment();
+//                frag.setArguments(bundle);
+//
+//                /*fragmentManager.setCustomAnimations(
+//                        R.anim.push_right_in,
+//                        R.anim.push_left_out, R.anim.push_left_in, R.anim.push_right_out);*/
+//                // update the main content by replacing fragments
+//                fragmentManager.beginTransaction()
+//                        .replace(R.id.container, frag)
+//                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+//                        .addToBackStack(null)
+//                        .commit();
+//            }
+//        });
 
         leaverequsLay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -244,6 +270,81 @@ public class DashBoardFragment extends Fragment {
                 Intent ik = new Intent(getActivity(),NewAddLeaveMangementActivity.class);
                 startActivity(ik);
                 getActivity().overridePendingTransition(R.anim.push_right_in, R.anim.push_left_out);
+
+            }
+        });
+
+        pieChart = (PieChart)rootView.findViewById(R.id.chart1);
+
+        barchart = (BarChart) rootView.findViewById(R.id.chart2);
+
+        pieChart11 = (PieChart)rootView.findViewById(R.id.chart11);
+
+        entries = new ArrayList<>();
+
+        entriesbar = new ArrayList<>();
+
+        PieEntryLabels = new ArrayList<String>();
+
+        BarEntryLable = new ArrayList<String>();
+
+        AddValuesToPIEENTRY();
+
+        AddValuesToBarENTRY();
+
+        AddValuesToPieEntryLabels();
+
+        AddValuesToBarEntryLabels();
+
+        pieDataSet = new PieDataSet(entries, "");
+        barDataSet = new BarDataSet(entriesbar,"");
+
+        pieData = new PieData(PieEntryLabels, pieDataSet);
+        barData = new BarData(BarEntryLable,barDataSet);
+
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        pieChart.setData(pieData);
+        pieChart11.setData(pieData);
+        barchart.setData(barData);
+
+        pieChart.animateY(3000);
+        pieChart11.animateY(3000);
+        barchart.animateY(3000);
+
+        pieChart.setHighlightPerTapEnabled(true);
+        pieChart11.setHighlightPerTapEnabled(true);
+        barchart.setHighlightPerTapEnabled(true);
+
+        pieChart.setCenterText("Machine Status");
+
+        pieChart.setDescription(null);
+
+        pieChart11.setDescription(null);
+
+        barchart.setDescription(null);
+
+        pieChart.setOnChartValueSelectedListener( new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
+                //fire up event
+
+                //   Toast.makeText(Dashboard.this, "item Clicked "+dataSetIndex +" "+ e, Toast.LENGTH_SHORT).show();
+                if(e.getXIndex() == 0){
+
+                    //    Toast.makeText(DashboardActivity.this, "item Clicked 0 "+dataSetIndex +" "+ e, Toast.LENGTH_SHORT).show();
+
+                } else if(e.getXIndex() == 1){
+
+                    //   Toast.makeText(DashboardActivity.this, "item Clicked 1"+dataSetIndex +" "+ e, Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected() {
 
             }
         });
@@ -267,5 +368,44 @@ public class DashBoardFragment extends Fragment {
     public interface OnFragmentInteractionListenerForToolbar {
         // TODO: Update argument type and name
         void onFragmentInteractionForToolbarMethod(int navigationCount, String Titile, String count);
+    }
+
+
+    public void AddValuesToPIEENTRY(){
+
+        entries.add(new BarEntry(2f, 0));
+        entries.add(new BarEntry(4f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(8f, 3));
+
+    }
+
+    public void AddValuesToPieEntryLabels(){
+
+        PieEntryLabels.add("AMC");
+        PieEntryLabels.add("Warranty");
+        PieEntryLabels.add("Paid");
+        PieEntryLabels.add("FOC");
+
+
+    }
+
+    public void AddValuesToBarENTRY(){
+        entriesbar.add(new BarEntry(7, 0));
+        entriesbar.add(new BarEntry(5, 1));
+        entriesbar.add(new BarEntry(0, 2));
+        entriesbar.add(new BarEntry(0, 3));
+        entriesbar.add(new BarEntry((float) 22.5, 4));
+        entriesbar.add(new BarEntry(17, 5));
+    }
+
+    public void AddValuesToBarEntryLabels(){
+        BarEntryLable.add("CL");
+        BarEntryLable.add("CL_Remain");
+        BarEntryLable.add("Comp.Off");
+        BarEntryLable.add("Comp.Off_Remain");
+        BarEntryLable.add("PL");
+        BarEntryLable.add("PL_Remain");
+
     }
 }
